a2acaec54cdf76ef9946ea101ae026f37266f45a
mdarifmia <arif@supremesupports.com.au>
2024-02-24 17:55:34 +0600

    update menu item design

--- src/component/Menu/MenuItems.tsx
+++ src/component/Menu/MenuItems.tsx
@@ -6,17 +6,23 @@ import { primaryColor } from "@/utils/color";
 const MenuItems = () => {
   const menuItems = [
     {
-      key: "1",
+      key: "/",
       label: "Home",
     },
   ];
   return (
     <Menu
       mode="horizontal"
-      defaultSelectedKeys={["2"]}
-      items={menuItems}
-      style={{ width: "100%", backgroundColor: primaryColor, color: "#fff" }}
-    />
+      defaultSelectedKeys={["home"]}
+      style={{ width: "100%" }}
+    >
+      <Menu.Item key="home">
+        <Link href={"/"}>Home</Link>
+      </Menu.Item>
+      <Menu.Item key="cart">
+        <Link href={"/cart"}>Cart</Link>
+      </Menu.Item>
+    </Menu>
   );
 };
 
